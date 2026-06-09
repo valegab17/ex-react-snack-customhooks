bab97c1bda70a03618ef2e86a4afa8f725070574
Valeria Gabrielli <gabvale53@gmail.com>
2026-06-09 18:43:16 +0200

    useDate()

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import useSwitch from './useSwitch'
+import useDate from "./useDate";
+
 function App() {
   const [isOn, toggle] = useSwitch();
-
+  const currentDate = useDate();
   return (
     <>
-        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-        <button onClick={toggle}>Cambia Stato</button>
-    
+      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+      <button onClick={toggle}>Cambia Stato</button>
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>{currentDate.toLocaleString()}</p>
+      </div>
     </>
   )
 }
--- src/useDate.jsx
+++ src/useDate.jsx
@@ -10,12 +10,17 @@ import { useState, useEffect } from "react"
 
 
 function useDate() {
-    const [date, setDate] = useState(()=> new Date().toLocaleDateString())
-    const [time, setTime] = useState(() => new Date().toLocaleTimeString())
-    useEffect(()=>{
-        
-    }, [])
-    return
-}
 
-export default useDate
+    const [currentDate, setCurrentDate] = useState(() => new Date());
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{
+          setCurrentDate(new Date());
+        
+        },1000)
+
+        //funzione di pulizia quando il compnente viene smontato
+        return ()=> clearInterval(intervalId);
+    }, []);
+    return currentDate;
+}
+export default useDate;
